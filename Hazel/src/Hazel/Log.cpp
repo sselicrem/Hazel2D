b178182a4f10b7a9baef56d29434afd8fb67732a
aleksei neustroev <sselicrem@gmail.com>
2026-05-14 15:41:29 +0300

    added Event system

--- Hazel/src/Hazel/Log.cpp
+++ Hazel/src/Hazel/Log.cpp
@@ -11,7 +11,7 @@ namespace Hazel {
 		s_CoreLogger = spdlog::stdout_color_mt("HAZEL");
 		s_CoreLogger->set_level(spdlog::level::trace);
 
-		s_CoreLogger = spdlog::stdout_color_mt("APP");
-		s_CoreLogger->set_level(spdlog::level::trace);
+		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger->set_level(spdlog::level::trace);
 	}
 }
--- premake5.lua
+++ premake5.lua
@@ -29,6 +29,7 @@ project "Hazel"
 
 	includedirs
 	{
+		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include"
 	}
 
