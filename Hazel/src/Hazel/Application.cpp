bb4d6266e7d9e59ab58920f6a6f8b7e1e4842839
aleksei neustroev <sselicrem@gmail.com>
2026-05-19 13:22:00 +0300

    moved OnAttach calls to LayerStack v2

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -65,13 +65,11 @@ namespace Hazel {
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
-		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* overlay)
 	{
 		m_LayerStack.PushOverlay(overlay);
-		overlay->OnAttach();
 	}
 
 	bool Application::OnWindowClose(Event& e)
