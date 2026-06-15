d1b11c7dc29f3a0c631c1003fe8540f1c6ca6ac5
aleksei neustroev <sselicrem@gmail.com>
2026-06-15 17:02:20 +0300

    Added initial texture support

--- Hazel/src/Hazel/Renderer/VertexArray.cpp
+++ Hazel/src/Hazel/Renderer/VertexArray.cpp
@@ -6,16 +6,16 @@
 
 namespace Hazel {
 
-	VertexArray* VertexArray::Create()
-	{
-		switch (Renderer::GetAPI())
+		VertexArray* VertexArray::Create()
 		{
-		case RendererAPI::API::None:	HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");	return nullptr;
-		case RendererAPI::API::OpenGL: return new OpenGLVertexArray();
+			switch (Renderer::GetAPI())
+			{
+			case RendererAPI::API::None:	HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");	return nullptr;
+			case RendererAPI::API::OpenGL: return new OpenGLVertexArray();
+			}
+			HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+			return nullptr;
 		}
-		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
-		return nullptr;
-	}
 
 
 }
--- premake5.lua
+++ premake5.lua
@@ -17,6 +17,7 @@ IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
 IncludeDir["imgui"] = "Hazel/vendor/imgui"
 IncludeDir["glm"] = "Hazel/vendor/glm"
+IncludeDir["stb_image"] = "Hazel/vendor/stb_image"
 
 include "Hazel/vendor/GLFW"
 include "Hazel/vendor/Glad"
@@ -40,6 +41,8 @@ project "Hazel"
 	{
 		"%{prj.name}/src/**.h",
 		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/stb_image/**.h",
+		"%{prj.name}/vendor/stb_image/**.cpp",
 		"%{prj.name}/vendor/glm/glm/**.hpp",
 		"%{prj.name}/vendor/glm/glm/**.inl"
 	}
@@ -51,7 +54,8 @@ project "Hazel"
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
 		"%{IncludeDir.imgui}",
-		"%{IncludeDir.glm}"
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.stb_image}"
 	}
 
 	links 
