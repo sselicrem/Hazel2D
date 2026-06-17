5902b4abd2864d345d20bb5ec2b4af048f680a58
aleksei neustroev <sselicrem@gmail.com>
2026-06-17 16:17:43 +0300

    minor fixes, update .gitmodules

--- Hazel/src/Hazel/Core/LayerStack.cpp
+++ Hazel/src/Hazel/Core/LayerStack.cpp
@@ -10,7 +10,10 @@ namespace Hazel {
 	LayerStack::~LayerStack()
 	{
 		for (auto layer : m_Layers)
+		{
+			layer->OnDetach();	
 			delete layer;
+		}
 	}
 
 	void LayerStack::PushLayer(Layer* layer)
