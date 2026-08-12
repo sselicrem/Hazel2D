2989273886a433421535d9dc59f9c5bb3a99ab80
aleksei neustroev <sselicrem@gmail.com>
2026-08-12 16:10:40 +0300

    added icons

--- Hazelnut/src/Panels/ContentBrowserPanel.cpp
+++ Hazelnut/src/Panels/ContentBrowserPanel.cpp
@@ -10,11 +10,13 @@ namespace Hazel {
 	ContentBrowserPanel::ContentBrowserPanel()
 		: m_CurrentDirectiry(s_AssetPath)
 	{
+		m_DirectoryIcon = Texture2D::Create("resources/icons/contentBrowser/DirectoryIcon.png");
+		m_FileIcon = Texture2D::Create("resources/icons/contentBrowser/FileIcon.png");
 	}
 
 	void ContentBrowserPanel::OnImGuiRender()
 	{
-		ImGui::Begin("Content Browser");
+		ImGui::Begin("Content Browser");	
 
 		if (m_CurrentDirectiry != s_AssetPath)
 		{
@@ -24,26 +26,38 @@ namespace Hazel {
 			}
 		}
 
+		static float padding = 8.f;
+		static float thumbnailSize = 64.f;
+		float cellSize = thumbnailSize + padding;
+
+		float panelWidth = ImGui::GetContentRegionAvail().x;
+		int columnCount = (int)(panelWidth / cellSize);
+		if (columnCount < 1)
+			columnCount = 1;
+
+		ImGui::Columns(columnCount, 0, false);
+
 		for (auto& directory: std::filesystem::directory_iterator(m_CurrentDirectiry))
 		{
 			const auto& path = directory.path();
 			auto relativePath = std::filesystem::relative(path, s_AssetPath);
-			std::string relativePathString = relativePath.string();
-			if (directory.is_directory())
+			std::string filenameString = relativePath.filename().string();
+
+			Ref<Texture2D> icon = directory.is_directory() ? m_DirectoryIcon : m_FileIcon;
+			ImGui::ImageButton((ImTextureID)icon->GetRendererID(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });
+			if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
 			{
-				if (ImGui::Button(relativePathString.c_str()))
+				if (directory.is_directory())
 				{
 					m_CurrentDirectiry /= path.filename();
 				}
+			}
+			ImGui::TextWrapped(filenameString.c_str());
 
-			}
-			else
-			{
-				if (ImGui::Button(relativePathString.c_str()))
-				{
-				}
-			}
+			ImGui::NextColumn();
 		}
+
+		ImGui::Columns(1);
 
 		ImGui::End();
 	}
--- Hazelnut/src/Panels/ContentBrowserPanel.h
+++ Hazelnut/src/Panels/ContentBrowserPanel.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include "Hazel/Renderer/Texture.h"
 
 namespace Hazel {
 
@@ -13,5 +14,8 @@ namespace Hazel {
 		void OnImGuiRender();
 	private:
 		std::filesystem::path m_CurrentDirectiry;
+
+		Ref<Texture2D> m_DirectoryIcon;
+		Ref<Texture2D> m_FileIcon;
 	};
 }
