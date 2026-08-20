1a5828609af37b082bf702fc313a38aa637be10f
aleksei neustroev <sselicrem@gmail.com>
2026-08-20 13:55:59 +0300

    Added line drawing

--- Hazel/src/Hazel/Renderer/RendererAPI.h
+++ Hazel/src/Hazel/Renderer/RendererAPI.h
@@ -22,6 +22,8 @@ namespace Hazel {
 		virtual void Clear() = 0;
 
 		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0) = 0;
+		virtual void DrawLines(const Ref<VertexArray>& vertexArray, uint32_t vertexCount = 0) = 0;
+		virtual void SetLineWidth(float lineWidth) = 0;
 
 		inline static API GetAPI() { return s_API; }
 		static Scope<RendererAPI> Create();
